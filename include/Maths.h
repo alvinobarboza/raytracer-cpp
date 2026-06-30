485b9a1fde67b05baeb13eef8868ec75742453a0
Alvino Barboza <alvinobarboza@gmail.com>
2026-06-30 10:34:56 -0300

    Unused function

--- include/Maths.h
+++ include/Maths.h
@@ -8,7 +8,6 @@ namespace Math {
     constexpr float DEG_TO_RAD = TAU / 360.0f;
     constexpr float RAD_TO_DEG = 360.0f / TAU;
     float lerp_to(float a, float b, float t);
-    float fresnel_schlick(const Vec3 &incidence, const Vec3 &normal, float refractive_index);
     float clamp(float x, float min, float max);
 }
 
